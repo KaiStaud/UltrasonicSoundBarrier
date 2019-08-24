d7ebf61d6683d23d78d251410e8ebbe2fa3995c1
Kai Staud <kai-staud@web.de>
2019-08-24 16:43:02 +0200

    Added Defines and Powermodes

---
+++ 3_BT_App/openIR/src/main/java/com/example/openir_controll/About.kt
@@ -0,0 +1,13 @@
+package com.example.openir_controll
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class About: AppCompatActivity()
+{
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_contact_information)
+    }
+}
